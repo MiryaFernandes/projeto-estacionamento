9bd0d54bc0ea354dece41e3bf1b18feef4fd63f5
miryã <db220355>
2022-05-27 16:58:30 -0300

    inicio tela de login

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">    <link rel="stylesheet" href="login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="center">
+        <h1>faça o login</h1>
+        <form method="post">
+            <div class="txt_field">
+                <label>email:</label>
+                <input type="text" required>
+            </div>
+            <div class="txt_field"> 
+                <label>senha:</label>
+                <input type="password" required> 
+            </div>
+
+            <div class="pass">esqueceu a senha?</div>      
+            <input type="submit" value="login">   
+            <div class="signup_link">
+                ainda não está cadastrado? <a href="#">se cadastre</a>
+            </div>    
+            
+        </form>
+    </div>
+</body>
+</html>
